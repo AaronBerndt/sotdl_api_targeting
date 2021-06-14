49b0103e423ceb9967c0c321b14c37438851708e
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-06-14 07:11:41 -0500

    Added insertIntoCollection

--- utilities/MongoUtils.ts
+++ utilities/MongoUtils.ts
@@ -23,3 +23,24 @@ export async function fetchCollection(collectionName: string) {
     await client.close();
   }
 }
+
+export async function insertIntoCollection(
+  collectionName: string,
+  newDocument: any
+) {
+  try {
+    if (uri === undefined) {
+      throw "URI is undefined";
+    }
+    await client.connect();
+    const database = client.db("sotdl");
+    const collection = database.collection(collectionName);
+    await collection.insertOne(newDocument);
+
+    return { message: `Sucessfully Added new Document into ${collection}` };
+  } catch (e) {
+    throw e;
+  } finally {
+    await client.close();
+  }
+}
