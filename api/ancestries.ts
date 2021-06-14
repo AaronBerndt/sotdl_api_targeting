cedbad5e6563849be5de23c555026e9d5b7b813f
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-06-14 16:17:52 -0500

    removed vercel

--- api/ancestries.ts
+++ api/ancestries.ts
@@ -4,7 +4,6 @@ import { fetchCollection } from "../utilities/MongoUtils";
 export default async (request: VercelRequest, response: VercelResponse) => {
   try {
     const data = await fetchCollection("ancestries");
-    console.log(data);
     response.status(200).send(data);
   } catch (e) {
     response.status(504).send(e);
