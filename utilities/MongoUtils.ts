84b8bce9fbdc268feca5138cda33bfaf267594b2
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-06-14 07:04:01 -0500

    Added Error Handling

--- utilities/MongoUtils.ts
+++ utilities/MongoUtils.ts
@@ -8,6 +8,9 @@ const client = new MongoClient(uri, {
 
 export async function fetchCollection(collectionName: string) {
   try {
+    if (uri === undefined) {
+      throw "URI is undefined";
+    }
     await client.connect();
     const database = client.db("sotdl");
     const collection = database.collection(collectionName);
