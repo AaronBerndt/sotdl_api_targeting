b2baa876d12e756603a4813c9cfde657e509e4b8
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-06-14 16:36:26 -0500

    Changed to use cache

--- utilities/MongoUtils.ts
+++ utilities/MongoUtils.ts
@@ -1,26 +1,35 @@
 import { MongoClient } from "mongodb";
 
+let cachedDb = null;
+
 const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const client = await MongoClient.connect(uri, { useNewUrlParser: true });
+
+  const db = client.db("sotdl");
+
+  cachedDb = db;
+  return db;
+}
 
 export async function fetchCollection(collectionName: string) {
   try {
     if (uri === undefined) {
       throw "URI is undefined";
     }
-    await client.connect();
-    const database = client.db("sotdl");
+
+    const database = await connectToDatabase();
     const collection = database.collection(collectionName);
     const data = await collection.find({}).toArray();
 
     return data;
   } catch (e) {
     throw e;
-  } finally {
-    await client.close();
   }
 }
 
@@ -32,15 +41,13 @@ export async function insertIntoCollection(
     if (uri === undefined) {
       throw "URI is undefined";
     }
-    await client.connect();
-    const database = client.db("sotdl");
+
+    const database = await connectToDatabase();
     const collection = database.collection(collectionName);
     await collection.insertOne(newDocument);
 
     return { message: `Sucessfully Added new Document into ${collection}` };
   } catch (e) {
     throw e;
-  } finally {
-    await client.close();
   }
 }
