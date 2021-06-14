56e9f71ecee5d543d02d19b59eb1913d48b7e7e0
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-06-14 06:08:53 -0500

    Added console.log

--- api/ancestries.ts
+++ api/ancestries.ts
@@ -3,5 +3,6 @@ import { fetchCollection } from "../utilities/MongoUtils";
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   const data = await fetchCollection("ancestries");
+  console.log(data);
   response.status(200).send(data);
 };
