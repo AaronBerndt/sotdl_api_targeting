44a5be5d55f098ffc8af8c4a030ebfc565d25e6d
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-06-15 16:15:32 -0500

    Added a bunch of apis

---
+++ api/insertIntoSpells.ts
@@ -0,0 +1,12 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { fetchCollection, insertIntoCollection } from "../utilities/MongoUtils";
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  try {
+    const { documents } = request.query;
+    const data = await insertIntoCollection("spells", documents);
+    response.status(200).send(documents);
+  } catch (e) {
+    response.status(504).send(e);
+  }
+};
---
+++ api/spells.ts
@@ -0,0 +1,11 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { fetchCollection } from "../utilities/MongoUtils";
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  try {
+    const data = await fetchCollection("spells");
+    response.status(200).send(data);
+  } catch (e) {
+    response.status(504).send(e);
+  }
+};
