0603bc97b5e66d35da328a4bcef4c66f22948586
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-06-20 20:59:16 -0500

    Added update apis

---
+++ api/updateAncestries.ts
@@ -0,0 +1,12 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { updateCollection } from "../utilities/MongoUtils";
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  try {
+    const { documents } = request.body;
+    const data = await updateCollection("ancestries", documents);
+    response.status(200).send(data);
+  } catch (e) {
+    response.status(504).send(e);
+  }
+};
