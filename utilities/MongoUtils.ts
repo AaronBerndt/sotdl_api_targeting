119025bc19dc4ae7741b19233dce3442d0edf194
AaronBerndt <aaron.berndt2@gmail.com>
2021-07-20 08:57:10 -0500

    Update MongoUtils.ts

--- utilities/MongoUtils.ts
+++ utilities/MongoUtils.ts
@@ -31,7 +31,7 @@ export async function insertIntoCollection(
 
     const database = await connectToDatabase();
     const collection = database.collection(collectionName);
-    await collection.insertMany(newDocuments, { ordered: true });
+    await collection.insertMany([newDocuments], { ordered: true });
 
     return { message: `Sucessfully Added new documents into ${collection}` };
   } catch (e) {
