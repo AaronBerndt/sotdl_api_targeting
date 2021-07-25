44f889c0a69b818a11cf4f3471a96e0dacd186ea
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-07-25 14:20:29 -0500

    Added insertIntoCombat

---
+++ api/insertIntoCombat.ts
@@ -0,0 +1,25 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import microCors from "micro-cors";
+import { insertIntoCollection } from "../utilities/MongoUtils";
+
+const cors = microCors();
+
+const handler = async (request: VercelRequest, response: VercelResponse) => {
+  try {
+    if (request.method === "OPTIONS") {
+      return response.status(200).end();
+    }
+    const data = await insertIntoCollection("combat", [
+      {
+        combatants: [],
+        turn: 0,
+        currentRound: "Play Fast",
+      },
+    ]);
+    response.status(200).send(data);
+  } catch (e) {
+    response.status(504).send(e);
+  }
+};
+
+export default cors(handler);
