bd4ec5ca5ce56fa6e6b773181ae62316b5925bd6
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-10-02 20:55:55 -0500

    Added Random

--- api/insertIntoCharactersRandom.ts
+++ api/insertIntoCharactersRandom.ts
@@ -3,6 +3,7 @@ import { insertIntoCollection } from "../utilities/MongoUtils";
 import { Character } from "../types";
 import microCors from "micro-cors";
 import axios from "axios";
+import { shuffle, take } from "lodash";
 
 const cors = microCors();
 
@@ -11,6 +12,8 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
     if (request.method === "OPTIONS") {
       return response.status(200).end();
     }
+
+    const getRandomInt = (max) => Math.floor(Math.random() * max);
 
     const { level } = request.body;
 
@@ -22,37 +25,54 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
       `https://sotdl-api-fetch.vercel.app/api/paths`
     );
 
-    const filterByLevel = (array) =>
-      array.filter(({ level: filterLevel }) => filterLevel <= level);
-
     const pickRandomAncestry = () => {
-      return { name };
+      const { name } = ancestries[getRandomInt(ancestries.length + 1)];
+
+      return name;
     };
 
-    const pickRandomPath = (type: string) => {
-      const [path] = paths.filter(
-        ({ type: filterType }) => filterType === type
-      );
+    const pickRandomPath = (pathType: string) => {
+      const filteredPaths = paths.filter(({ type }) => type === pathType);
 
-      return { name };
+      const { name } = filteredPaths[getRandomInt(filteredPaths.length + 1)];
+
+      return name;
     };
 
-    const pickRandomCharacteristics = () => {
-      return [];
+    const pickRandomCharacteristics = (level: number) => {
+      const statList = ["Strength", "Agility", "Will", "Intellect"];
+
+      const createRandomCharacteristicsList = (amount, atLevel) =>
+        take(shuffle(statList), amount).map((characteristic) => ({
+          id: `${characteristic}-${atLevel}`,
+          name: characteristic,
+          value: 1,
+          level: atLevel,
+        }));
+
+      const noviceList =
+        level >= 1 ? createRandomCharacteristicsList(2, 1) : [];
+      const expertList =
+        level >= 1 ? createRandomCharacteristicsList(2, 3) : [];
+      const masterList =
+        level >= 1 ? createRandomCharacteristicsList(3, 7) : [];
+
+      return [...noviceList, ...expertList, ...masterList];
     };
 
-    const newCharacterData: Character = {
+    const ancestry = pickRandomAncestry();
+    const newCharacterData: any = {
       name: "",
       level: level,
-      ancestry: "",
-      novicePath: level <= 1 ? "" : "",
-      expertPath: level <= 3 ? "" : "",
-      masterPath: level <= 7 ? "" : "",
-      characteristics: [
-        ...filterByLevel(ancestries[0].characteristics),
-        ...pickRandomCharacteristics(),
-      ],
-      talents: [...filterByLevel(ancestries[0].talents)],
+      ancestry,
+      novicePath:
+        level >= 1 && ancestry !== ("Jotun" || "Centaur")
+          ? pickRandomPath("Novice")
+          : "",
+      expertPath: level >= 3 ? pickRandomPath("Expert") : "",
+      masterPath: level >= 7 ? pickRandomPath("Master") : "",
+      characteristics: pickRandomCharacteristics(level),
+      talents: [],
       spells: [],
       traditions: [],
       items: {
@@ -66,8 +86,8 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
           gold: 0,
         },
       },
-      languages: ["Common", "Dwarf"],
-      professions: [{ name: "Guard", type: "Martial" }],
+      languages: ["Common"],
+      professions: [],
       details: [],
       choices: [],
       characterState: {
