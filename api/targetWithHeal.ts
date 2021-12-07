855430290d9d22237533e11d2f508fff7da9986d
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-12-06 20:41:04 -0600

    Finished targetWithHeal

--- api/targetWithHeal.ts
+++ api/targetWithHeal.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { insertIntoCollection } from "../utilities/MongoUtils";
 import { UPDATE_CHARACTER_HEALTH_URL } from "../utilities/api.config";
 import microCors from "micro-cors";
 
@@ -13,24 +12,31 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
     }
     const { targetArray, abilityName, healingFactor } = request.body.data;
 
-    await Promise.all(
-      targetArray.map(async (target) => {
+    const data = await Promise.all(
+      targetArray.map(async (target: string) => {
+        console.log(target);
         const { data } = await axios(
           `https://sotdl-api-fetch.vercel.app/api/characters?_id=${target}`
         );
 
-        const { HealingRate } = data.characteristic;
+        const { HealingRate } = data.characteristics;
 
         const healthChangeAmount = HealingRate * healingFactor;
 
         await axios.post(UPDATE_CHARACTER_HEALTH_URL, {
-          data: { healthChangeAmount, _id: target },
+          data: { healthChangeAmount: -healthChangeAmount, _id: target },
         });
+
+        return {
+          name: data.name,
+          healedAmount: healthChangeAmount,
+        };
       })
     );
 
-    response.status(200).send("Made attack against Targets");
+    response.status(200).send(data);
   } catch (e) {
+    console.log(e);
     response.status(504).send(e);
   }
 };
