8345c2ef5e22f450e555d0f9efd84d07bae03195
AaronBerndt <aaron_berndt@DragonSlayer.localdomain>
2021-12-06 20:52:44 -0600

    Added targetWithTemporaryEffect

--- api/targetWithTemporaryEffect.ts
+++ api/targetWithTemporaryEffect.ts
@@ -1,5 +1,6 @@
+import axios from "axios";
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { insertIntoCollection } from "../utilities/MongoUtils";
+import { UPDATE_TEMPORARYEFFECTS_URL } from "../utilities/api.config";
 import microCors from "micro-cors";
 
 const cors = microCors();
@@ -9,10 +10,30 @@ const handler = async (request: VercelRequest, response: VercelResponse) => {
     if (request.method === "OPTIONS") {
       return response.status(200).end();
     }
-    const { targetArray, abilityName } = request.body.data;
+    const { targetArray, temporaryEffectName } = request.body.data;
 
-    response.status(200).send("Made attack against Targets");
+    const data = await Promise.all(
+      targetArray.map(async (target: string) => {
+        const { data } = await axios(
+          `https://sotdl-api-fetch.vercel.app/api/characters?_id=${target}`
+        );
+
+        await axios.post(UPDATE_TEMPORARYEFFECTS_URL, {
+          temporaryEffect: temporaryEffectName,
+          action: "add",
+          _id: target,
+        });
+
+        return {
+          name: data.name,
+          temporaryEffectAdd: temporaryEffectName,
+        };
+      })
+    );
+
+    response.status(200).send(data);
   } catch (e) {
+    console.log(e);
     response.status(504).send(e);
   }
 };
